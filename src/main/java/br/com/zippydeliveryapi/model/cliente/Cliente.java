92cfcb0871a0844570a14e6446018c54b2375499
darllen <jamillymilly10@hotmail.com>
2023-10-31 18:53:50 -0300

    a

---
+++ src/main/java/br/com/zippydeliveryapi/model/cliente/Cliente.java
@@ -0,0 +1,55 @@
+package br.com.zippydeliveryapi.model.cliente;
+
+import org.hibernate.annotations.Where;
+
+import br.com.zippydeliveryapi.util.entity.EntidadeAuditavel;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Cliente")
+@Where(clause = "habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Cliente extends EntidadeAuditavel {
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(unique = false)
+    private String cpf;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String senha;
+
+    @Column
+    private String logradouro;
+
+    @Column
+    private String bairro;
+
+    @Column
+    private String cidade;
+
+    @Column
+    private String estado;
+
+    @Column
+    private String cep;
+
+    @Column
+    private String complemento;
+
+}
