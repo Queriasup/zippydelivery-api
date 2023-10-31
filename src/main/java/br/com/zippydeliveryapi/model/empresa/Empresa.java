9e8c4ba9046859b275dab4be626aae6a4211699d
darllen <jamillymilly10@hotmail.com>
2023-10-31 18:39:53 -0300

    add campo senha para validar no mobile

---
+++ src/main/java/br/com/zippydeliveryapi/model/empresa/Empresa.java
@@ -0,0 +1,74 @@
+package br.com.zippydeliveryapi.model.empresa;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+import org.hibernate.annotations.Where;
+
+import br.com.zippydeliveryapi.util.entity.EntidadeAuditavel;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "Empresa")
+@Where(clause = "habilitado = true")
+@Builder
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Empresa extends EntidadeAuditavel {
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(unique = true)
+    private String cnpj;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column
+    private String categoria;
+
+    @Column
+    private String horario;
+
+    @Column
+    private Integer tempoEntrega;
+
+    @Column
+    private Double taxaFrete;
+
+    @Column(nullable = false, unique = true)
+    private String telefone;
+
+    @Column
+    private String imgPerfil;
+
+    @Column
+    private String imgCapa;
+
+    @Column
+    private String logradouro;
+
+    @Column
+    private String bairro;
+
+    @Column
+    private String cidade;
+
+    @Column
+    private String estado;
+
+    @Column(nullable = false, length = 10)
+    private String cep;
+
+    @Column
+    private String complemento;
+
+}
