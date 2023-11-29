4896af2dd211b83ffb2e42be4b1fe2f6d71f6cc1
aluno <aluno@ifpe>
2023-11-29 11:11:25 -0300

    rota get cliente por user

--- src/main/java/br/com/zippydeliveryapi/model/categoria/CategoriaProdutoRepository.java
+++ src/main/java/br/com/zippydeliveryapi/model/categoria/CategoriaProdutoRepository.java
@@ -4,8 +4,6 @@ import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import br.com.zippydeliveryapi.model.produto.Produto;
-
 public interface CategoriaProdutoRepository extends JpaRepository<CategoriaProduto, Long> {
     
     List<CategoriaProduto> findByEmpresaId(Long idEmpresa);
--- src/main/java/br/com/zippydeliveryapi/model/cliente/ClienteRepository.java
+++ src/main/java/br/com/zippydeliveryapi/model/cliente/ClienteRepository.java
@@ -1,7 +1,7 @@
 package br.com.zippydeliveryapi.model.cliente;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
+
 import br.com.zippydeliveryapi.model.acesso.Usuario;
 
 
